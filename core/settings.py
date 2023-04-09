219693f4ca98362899142a17912b6c1f282b227c
nurbekusenovvv <unurbek499@gmail.com>
2023-04-09 02:55:02 -0400

    third commit

--- core/settings.py
+++ core/settings.py
@@ -20,7 +20,7 @@ BASE_DIR = Path(__file__).resolve().parent.parent
 # See https://docs.djangoproject.com/en/4.2/howto/deployment/checklist/
 
 # SECURITY WARNING: keep the secret key used in production secret!
-SECRET_KEY = 'django-insecure-_*uu5_jcj829(hpd1$90ag%g9b4-q$2i8w95xw!oc%rdzdrbx@'
+SECRET_KEY = 'django-insecure-9=81_f66yb+po(o5z$oqa9c_x=1=du55wixianipxsado7q@$b'
 
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = True
@@ -37,6 +37,7 @@ INSTALLED_APPS = [
     'django.contrib.sessions',
     'django.contrib.messages',
     'django.contrib.staticfiles',
+    'cars'
 ]
 
 MIDDLEWARE = [
--- core/urls.py
+++ core/urls.py
@@ -18,8 +18,11 @@ from django.contrib import admin
 from django.urls import path
 from django.conf.urls.static import static
 from django.conf import settings
+from cars.views import index
 
 urlpatterns = [
     path('admin/', admin.site.urls),
+    path('', index, name='index'),
 ]
+
 urlpatterns += static(settings.MEDIA_URL,document_root = settings.MEDIA_ROOT)
