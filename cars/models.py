5efdca0071a2104f3e22900bde88222a2bf2dfab
nurbekusenovvv <unurbek499@gmail.com>
2023-04-09 02:51:04 -0400

    second commit

--- cars/models.py
+++ cars/models.py
@@ -1,3 +1,25 @@
 from django.db import models
 
-# Create your models here.
+class Car(models.Model):
+    number = models.CharField(
+        max_length=10,
+        verbose_name="Номер машины"
+
+    )
+    make = models.CharField(
+        max_length=50,
+        verbose_name="Производство"
+
+        )
+    model = models.CharField(
+        max_length=50,
+        verbose_name="Модель"
+
+        )
+    year = models.CharField(
+        max_length=50,
+        verbose_name="Год производства"
+    )
+
+    def __str__(self):
+        return self.number
--- cars/views.py
+++ cars/views.py
@@ -1,3 +1,14 @@
 from django.shortcuts import render
+from cars.models import Car
 
 # Create your views here.
+
+def index(request):
+    number = request.POST.get('number')
+    car = None
+    if number:
+        try:
+            car = Car.objects.get(number=number)
+        except Car.DoesNotExist:
+            pass
+    return render(request, 'index.html', {'number': number, 'car': car})
